cafc2904aee5e68ed5fa16dfa98fbb6376144a63
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-09 17:34:44 +0300

    make columns reactive

--- early-bird/src/components/landing-page/LandingPage.js
+++ early-bird/src/components/landing-page/LandingPage.js
@@ -3,8 +3,8 @@ import logo from '../../illustrations/Saly-landing-page.svg';
 
 function LandingPage() {
     return (
-        <div className="center horizontal">
-            <div className="left-elem">
+        <div className="row center horizontal">
+            <div className="col-xl-9 col-lg-8 col-sm-7">
                 <h1 id="title">Because the <span className="red">early birds</span> get the worm 💸</h1>
                 <p id="description">Too much time on your hands? Parents stressing you to get a job?
                 Want to look fly but the sneakers are too expensive?
@@ -12,7 +12,7 @@ function LandingPage() {
                 <button>Learn More</button>
                 <button>Start your hustle</button>
             </div>
-            <div>
+            <div className="col-xl-3 col-lg-4 col-sm-5">
                 <img className="logo" src={logo}></img>
             </div>
         </div>
