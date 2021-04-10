e7d13e2e7caac30e135daf6eb3389be0d519140b
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-10 20:38:47 +0300

    added css

--- early-bird/src/components/how-it-works/HowItWorks.js
+++ early-bird/src/components/how-it-works/HowItWorks.js
@@ -2,31 +2,32 @@ import hand from '../../illustrations/how-it-works/hand.svg';
 import searchIcon from '../../illustrations/how-it-works/search-icon.svg';
 import explosion from '../../illustrations/how-it-works/explosion.svg';
 import emoji from '../../illustrations/how-it-works/emoji.svg';
+import './HowItWorks.scss';
 
 function HowItWorks(){
     return (
-        <div class="center">
+        <div className="center-how-it-works">
             <h2>How it works</h2>
-            <div class="grid">
-                <div class="grid-item">
+            <div className="grid">
+                <div className="grid-item">
                     <img src={hand}></img>
                     <h4>Set up your account</h4>
-                    <p>Create an account with your personal information</p>
+                    <p className="hiw-text">Create an account with your personal information</p>
                 </div>
-                <div class="grid-item">
+                <div className="grid-item">
                     <img src={searchIcon}></img>
                     <h4>Find a good offer</h4>
-                    <p>Search for an offer you won’t be able to refuse</p>
+                    <p className="hiw-text">Search for an offer you won’t be able to refuse</p>
                 </div>
-                <div class="grid-item">
+                <div className="grid-item">
                     <img src={explosion}></img>
                     <h4>Do the tasks</h4>
-                    <p>Put in the work, but always enjoy your hustle</p>
+                    <p className="hiw-text">Put in the work, but always enjoy your hustle</p>
                 </div>
-                <div class="grid-item">
+                <div className="grid-item">
                     <img src={emoji}></img>
                     <h4>Profit</h4>
-                    <p>Collect your remuneration</p>
+                    <p className="hiw-text">Collect your remuneration</p>
                 </div>
             </div>
         </div>
