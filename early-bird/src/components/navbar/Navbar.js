7bdcbdff46153fee2a5e39aad578a183ce139395
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-11 10:20:31 +0300

    finished phone view

--- early-bird/src/components/navbar/Navbar.js
+++ early-bird/src/components/navbar/Navbar.js
@@ -4,14 +4,14 @@ function Navbar(){
     return(
         <div id="navbar">
             <div id="center-nav" className="row my-3">
-                <div id="links-left" className="col-md-4">
+                <div id="links-left" className="col-md-4 col-0">
                     <a className="text-bold text-black m-0 mr-4 ">About</a>
                     <a className="text-bold text-black m-0">Contact</a>
                 </div>
-                <h3 id="title-center" className="col-md-4 text-bold">Early Bird</h3>
-                <div id="btn-right" className="col-md-4">
-                    <button className="round bg-pink text-red text-bold px-4 py-2">Login</button>
-                    <button className="round bg-red text-white px-4 py-2 ml-3">Sign up</button>
+                <h3 id="title-center" className="col-md-4 col-6 text-bold">Early Bird</h3>
+                <div id="btn-right" className="col-md-4 col-6">
+                    <button className="round bg-pink text-red text-bold text-small px-4 py-1">Login</button>
+                    <button id="signup" className="round bg-red text-white text-small px-4 py-1">Sign up</button>
                 </div>
             </div>
         </div>
