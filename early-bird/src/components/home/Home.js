1ba13231edf061d49058cffb5c1474e87f2b9953
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-17 22:50:53 +0300

    started working on filter and sort

--- early-bird/src/components/home/Home.js
+++ early-bird/src/components/home/Home.js
@@ -1,10 +1,15 @@
 import "./Home.scss";
 import Hello from "./hello/Hello";
+import FilterAndSort from "./filter-and-sort/FilterAndSort";
+
+import {useState} from "react";
 
 function Home(){
+    const [offers, setOffers] = useState([]);
     return(
         <div>
             <Hello name="Cristian" role="worker"></Hello>
+            <FilterAndSort setOffers={setOffers}></FilterAndSort>
         </div>
     );
 }
