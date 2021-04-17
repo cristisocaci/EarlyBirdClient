47e528993ae7f78a665ec0e217b473173c6f75fa
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-17 18:27:25 +0300

    created home component

--- early-bird/src/App.js
+++ early-bird/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import LandingPage from './components/landing-page/LandingPage';
 import HowItWorks from './components/how-it-works/HowItWorks';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
 
 import React from "react";
@@ -20,7 +21,10 @@ function App() {
           <Route path="/login" exact>
               <Navbar page="login-page"></Navbar>
           </Route>
-
+          <Route path="/home" exact>
+              <Navbar page="main"></Navbar>
+              <Home></Home>
+          </Route>
           <Route path="/" exact>
             <Navbar page="landing-page"></Navbar>
             <LandingPage></LandingPage>
