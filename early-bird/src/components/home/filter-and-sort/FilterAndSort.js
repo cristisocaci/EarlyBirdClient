ee6fe10c81f30e70c16a6dbd7483d5cb3204b02f
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-18 17:18:43 +0300

    refactor

--- early-bird/src/components/home/filter-and-sort/FilterAndSort.js
+++ early-bird/src/components/home/filter-and-sort/FilterAndSort.js
@@ -1,58 +1,20 @@
 import "./FilterAndSort.scss";
-import { React, useState, useRef } from "react";
-import arrowClosed from "../../../illustrations/filter-and-sort/arrow-closed.svg";
+import { React, useState} from "react";
 import { GetAllUsers } from "../../../services/OffersService.js";
-
+import FilterDropdown from "./filter-dropdown/FilterDropdown";
 
 
 function FilterAndSort() {
-    const Cities = ["Cluj", "Bucuresti", "Oradea"];
-    GetAllUsers({cities: Cities});
+    const cities = ["Mojo", "Lubei"];
     const [query, setQuery] = useState({});
-    let filterPressed = false;
-    const filterRef = useRef();
 
-    function toggle(event) {
-        if (!filterPressed) {
-            filterRef.current.style.display = "block";
-            filterPressed = true;
-        }
-        else if(filterPressed && !event.target.closest(".fas-filter-content")) {
-            filterRef.current.style.display = "none";
-            filterPressed = false;
-        }
-    }
-    document.addEventListener("mouseup", function(event){
-        if (filterPressed && 
-            !event.target.closest(".fas-filter-dropdown")) {
-
-            filterRef.current.style.display = "none";
-            filterPressed = false;
-        }
-    });
-    
     return (
         <div>
             <div className="fas-search">
                 <input className="form-control fas-input"></input>
                 <button className="bg-red text-white round py-2 px-4">Search Offers</button>
             </div>
-            <div className="fas-filter-dropdown" onClick={toggle}>
-                <div className="fas-dropdown-display">
-                    <p className="m-0" >Filter by</p>
-                    <img src={arrowClosed} className="fas-icon"></img>
-                </div>
-                <div className="fas-filter-content"  ref={filterRef}>
-                <div className="divider"></div>
-                    <p>Min Cost</p>
-                    <input type="number" className="form-control"></input>
-                    <div className="divider"></div>
-                    <p>Max Cost</p>
-                    <input type="number" className="form-control"></input>
-                    <div className="divider"></div>
-                    <p>ddd</p>
-                </div>
-            </div>
+            <FilterDropdown></FilterDropdown>
 
         </div>
     );
---
+++ early-bird/src/components/home/filter-and-sort/filter-dropdown/FilterDropdown.js
@@ -0,0 +1,60 @@
+import "./FilterDropdown.scss";
+import arrowDown from "../../../../illustrations/filter-and-sort/arrow-down.svg";
+import { useRef } from "react";
+
+function FilterDropdown(){
+    let filterPressed = false;
+    const filterRef = useRef();
+    const btnRef = useRef();
+
+    function toggle(event) {
+        if (!filterPressed) {
+            filterRef.current.style.display = "block";
+            btnRef.current.style.transform = "rotate(-180deg)";
+            filterPressed = true;
+        }
+        else if(filterPressed && !event.target.closest(".fas-filter-content")) {
+            filterRef.current.style.display = "none";
+            btnRef.current.style.transform = "rotate(0deg)";
+            filterPressed = false;
+        }
+    }
+    document.addEventListener("mouseup", function(event){
+        if (filterPressed && 
+            !event.target.closest(".fas-filter-dropdown")) {
+
+            filterRef.current.style.display = "none";
+            btnRef.current.style.transform = "rotate(0deg)";
+            filterPressed = false;
+        }
+    });
+    
+    return(
+        <div className="fas-filter-dropdown" onClick={toggle}>
+                <div className="fas-dropdown-display">
+                    <p className="m-0" >Filter by</p>
+                    <img src={arrowDown} className="fas-icon" ref={btnRef}></img>
+                </div>
+                <div className="fas-filter-content"  ref={filterRef}>
+                <div className="divider"></div>
+                    <p>Min Cost</p>
+                    <input type="number" className="form-control"></input>
+                    <div className="divider"></div>
+                    <p>Max Cost</p>
+                    <input type="number" className="form-control"></input>
+                    <div className="divider"></div>
+                    <button className="fas-filter-btn round">
+                        City
+                        <img src={arrowDown} className="fas-icon-btn"></img>
+                    </button>
+                    <div className="divider"></div>
+                    <button className="fas-filter-btn round mb-2">
+                        Category
+                        <img src={arrowDown} className="fas-icon-btn"></img>
+                    </button>
+                </div>
+            </div>
+    )
+}
+
+export default FilterDropdown;
