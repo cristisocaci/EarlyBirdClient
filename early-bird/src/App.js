bd20067d6cd5c4f7adb802f8254512802f9be3ca
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-19 14:39:34 +0300

    landing page buttons

--- early-bird/src/App.js
+++ early-bird/src/App.js
@@ -30,6 +30,7 @@ function App() {
           <Route path="/" exact>
             <Navbar page="landing-page"></Navbar>
             <LandingPage></LandingPage>
+            <div id="hiw-scroll"></div>
             <HowItWorks></HowItWorks>
             <KeyFeatures></KeyFeatures>
             <CallToAction></CallToAction>
