a881c86d473c0afbff881694b60c53acd617c881
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-20 21:02:39 +0300

    about offer

--- early-bird/src/components/offers/about-offer/AboutOffer.js
+++ early-bird/src/components/offers/about-offer/AboutOffer.js
@@ -5,28 +5,45 @@ function AboutOffer(props){
     function renderCategories(){
         if(props.categories==null) return;
         return props.categories.map((x,index) =>
-            <span key={index}>{x.name}</span>
+            <span key={index} className="bg-red text-white category-pill">{x.name}</span>
         )
     }
+
+    function renderLocation(){
+        if(props.location==null) return;
+        return <p>{props.location.streetName} {props.location.streetNumber}, {props.location.cityName}</p>
+
+    }
     return (
-        <div>
-            <div>
-                <div>
-                    <h2>{props.title}</h2>
+        <div className="about-offer">
+            <div className="ao-upper">
+                <div className="ao-title-and-cost">
                     <div>
-                        {renderCategories()}
+                        <h2 className="text-bold">{props.title}</h2>
+                        <div className="ao-categories">
+                            {renderCategories()}
+                        </div>
+                    </div>
+                    <div className="ao-center-cost">
+                        <div className="ao-cost">
+                            <h2 className="text-red m-0">{props.cost}$</h2>
+                        </div>
                     </div>
                 </div>
+                <h4 className="text-bold">Description of the task:</h4>
+                <p>{props.description}</p>
+                
+            </div>
+            <div className="ao-lower">
                 <div>
-                    <h2>{props.cost}$</h2>
+                    <h4 className="text-bold">Prerequisites:</h4>
+                    <p>{props.prerequisites}</p>
+                </div>
+                <div>
+                    <h4 className="text-bold">Location:</h4>
+                    {renderLocation()}
                 </div>
             </div>
-            <h4>Description of the task:</h4>
-            <p>{props.description}</p>
-            <h4>Prerequisites:</h4>
-            <p>{props.prerequisites}</p>
-            <h4>Location:</h4>
-            <p>{props.location.streetName} {props.location.streetNumber}, {props.location.cityName}</p>
         </div>
     );
 }
