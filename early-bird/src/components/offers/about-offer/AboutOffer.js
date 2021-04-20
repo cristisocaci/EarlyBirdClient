af64db9a6a7f308c639b93e5eff6ee8856cb725b
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-20 20:07:17 +0300

    add html structure

--- early-bird/src/components/offers/about-offer/AboutOffer.js
+++ early-bird/src/components/offers/about-offer/AboutOffer.js
@@ -1,9 +1,32 @@
 import "./AboutOffer.scss";
 
-function AboutOffer(){
+function AboutOffer(props){
+
+    function renderCategories(){
+        if(props.categories==null) return;
+        return props.categories.map((x,index) =>
+            <span key={index}>{x.name}</span>
+        )
+    }
     return (
         <div>
-            
+            <div>
+                <div>
+                    <h2>{props.title}</h2>
+                    <div>
+                        {renderCategories()}
+                    </div>
+                </div>
+                <div>
+                    <h2>{props.cost}$</h2>
+                </div>
+            </div>
+            <h4>Description of the task:</h4>
+            <p>{props.description}</p>
+            <h4>Prerequisites:</h4>
+            <p>{props.prerequisites}</p>
+            <h4>Location:</h4>
+            <p>{props.location.streetName} {props.location.streetNumber}, {props.location.cityName}</p>
         </div>
     );
 }
