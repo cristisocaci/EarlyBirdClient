83245b133e481ccfc4456f1373c221926bb21821
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-20 16:38:48 +0300

    add helper functions

--- early-bird/src/services/AccountService.js
+++ early-bird/src/services/AccountService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 async function Login(username, password) {
   let path = sessionStorage.getItem("server") + "/api/Login";
@@ -7,7 +8,6 @@ async function Login(username, password) {
       username: username,
       password: password,
     });
-    console.log("cusescc");
     localStorage.setItem("jwt", response.data["token"]);
     return true; 
   } catch  {
@@ -15,4 +15,37 @@ async function Login(username, password) {
   }
 }
 
-export { Login };
+function IsUserLoggedIn(){
+  let jwt = localStorage.getItem("jwt");
+  if(jwt === null)
+    return false;
+  return true;
+
+  // the below code is used to check expiration date
+  // let jwtDecoded = jwt_decode(jwt);
+  // let isExpired = jwtDecoded.exp < new Date().getTime();
+  // return !isExpired;
+}
+
+
+function GetRole(){
+  let jwt = jwt_decode(localStorage.getItem("jwt"));
+  console.log(jwt);
+  if(jwt === null)
+    return null;
+  if(jwt.Admin === "true")
+    return "admin"
+  if(jwt.Worker === "true")
+    return "worker"
+  if(jwt.Publisher === "true")
+    return "publisher"
+  return null;
+}
+
+async function GetUserName(){
+  let jwt = jwt_decode(localStorage.getItem("jwt"));
+  if(jwt == null) return null;
+  return jwt.userName;
+}
+
+export { Login, IsUserLoggedIn, GetRole, GetUserName };
