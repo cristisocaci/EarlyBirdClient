a0bdbb630c1d39fbeb7da0f4053563c24aed77f2
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-22 20:41:28 +0300

    added user profile

--- early-bird/src/components/user-profile/UserProfile.js
+++ early-bird/src/components/user-profile/UserProfile.js
@@ -1,9 +1,49 @@
 import "./UserProfile.scss";
+import {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {GetUserById} from "../../services/UsersService";
+import {GetUserId, GetRole} from "../../services/AccountService";
+import UserCard from "../user-card/UserCard";
+import Reviews from "./reviews/Reviews";
 
 function UserProfile(){
-    return(
-        <div>
+    const [user, setUser] = useState({});
+    const [reviews, setReviews] = useState([]);
+    const [view, setView] = useState("");
+    let {id} = useParams();
 
+    useEffect(()=>{
+        async function fetchData(){
+            let u = await GetUserById(id);
+            if(u == null) {
+                window.location.href = "/404"
+                return;
+            }
+            let loggedInUserId = GetUserId();
+            let loggedInUserRole = GetRole();
+
+            if(loggedInUserId === id)
+                setView("owner");
+            else{
+                if(loggedInUserRole === u.role)
+                    window.location.href="/404"
+                setView("viewer");
+            }
+            setUser(u);
+        }
+        fetchData();
+    }, [id]);
+
+    return(
+        <div className="center-user-profile">
+            <div className="user-profile__card">
+                <UserCard
+                    user = {user}
+                    />
+            </div>
+            <div className="user-profile__reviews">
+                <Reviews></Reviews>
+            </div>
         </div>
     );
 }
