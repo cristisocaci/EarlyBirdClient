22b933de5075d1681cad049bd8957a5111eb195d
Cristian Nicolae <nicolae.cristianmihai@gmail.com>
2021-05-09 16:03:00 +0300

    Added all forms

--- early-bird/src/components/home/new-offer/NewOffer.js
+++ early-bird/src/components/home/new-offer/NewOffer.js
@@ -1,44 +1,120 @@
 import "./NewOffer.scss";
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import "date-fns";
+import { useEffect, useState } from "react";
+import { GetAllCategories } from "../../../services/CategoriesService";
 
 export default function NewOffer(props) {
+  const [category, setCategory] = useState({});
+  useEffect(() => {
+    async function fetchData() {
+      let o = await GetAllCategories();
+      if (o == null) {
+        window.location.href = "/404";
+        return;
+      }
+      setCategory(o);
+      console.log(o);
+    }
+    fetchData();
+  });
+
+  function renderCategories() {
+    if (props.categories == null) return;
+    return props.categories.map((x, index) => (
+      <span key={index} className="bg-red text-white category-pill">
+        {x.name}
+      </span>
+    ));
+  }
+
+  const [selectedDate, setSelectedDate] = React.useState(
+    Date().toLocaleString()
+  );
   const dialogRef = React.useRef(null);
   const handleClose = () => {
     props.setOpen(false);
   };
 
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
-      <Dialog ref={dialogRef} open={props.open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle ref={dialogRef} id="form-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText ref={dialogRef}>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
+    <Dialog
+      className="new-offer-dialog"
+      ref={dialogRef}
+      open={props.open}
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle ref={dialogRef} id="form-dialog-title">
+        Publish a new offer
+      </DialogTitle>
+      <DialogContent className="new-offer-content">
+        <div className="new-offer-top-forms">
           <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
+            id="new-offer-title"
+            variant="outlined"
+            className="new-offer-form"
           />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
-          </Button>
-        </DialogActions>
-      </Dialog>
+          <TextField
+            id="new-offer-description"
+            label="Description"
+            variant="outlined"
+            className="new-offer-description-form"
+          />
+        </div>
+        <div className="new-offer-bottom-part">
+          <div className="new-offer-bottom-forms">
+            <TextField
+              id="new-offer-prerequisites"
+              label="Prerequisites"
+              variant="outlined"
+              className="new-offer-form"
+            />
+            <TextField
+              id="new-offer-location"
+              label="Location"
+              variant="outlined"
+              className="new-offer-form"
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                variant="outlined"
+                format="dd/MM/yyyy"
+                id="date-picker-inline"
+                label="Deadline"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date",
+                }}
+              />
+            </MuiPickersUtilsProvider>
+          </div>
+          <div className="new-offer-categories">{category.name}</div>
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleClose} color="primary">
+          Publish
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
