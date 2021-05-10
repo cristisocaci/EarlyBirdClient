55adb296293b3f774cbaaddb46c89625f811bf1a
flaviuvr <flav.raita@gmail.com>
2021-05-10 18:58:10 +0300

    Update spacing on card component

--- early-bird/src/components/home/display-offers/card/Card.js
+++ early-bird/src/components/home/display-offers/card/Card.js
@@ -6,7 +6,7 @@ function Card(props){
     function redirectTo(page){
         history.push(page);
     }
-    
+
     return(
         <div className="card-element" onClick={() => redirectTo(`/offers/${props.offerId}`)}>
             <div className="card-upper">
@@ -19,11 +19,13 @@ function Card(props){
                     <span>{ props.publisher.firstname } </span>
                     <span>{ props.publisher.lastname }</span>
                 </div>
-                {
-                    props.categories.map((category, index) => (
-                        <span className="categories bg-red round text-white px-2 py-1 m-1" key={index}>{category.category.name}</span>
-                    ))
-                }
+                <div className="categories-grid">
+                    {
+                        props.categories.map((category, index) => (
+                            <span className="bg-red round text-white px-2 py-1 m-1" key={index}>{category.category.name}</span>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
